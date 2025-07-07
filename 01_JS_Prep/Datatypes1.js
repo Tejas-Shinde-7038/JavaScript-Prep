7b4b745a4bb5c44854acbc4960c3e75fca5785ad
Tejas-Shinde-7038 <tejasshinde7038@gmail.com>
2025-07-07 10:11:21 +0000

    sync successfully

---
+++ 01_JS_Prep/Datatypes1.js
@@ -0,0 +1,7 @@
+let no = 1234
+let str = "Tejas"
+let bool = "true"
+let value = null
+
+
+console.log(no,str,bool,value)
---
+++ 01_JS_Prep/Varaibles.js
@@ -0,0 +1,12 @@
+const AccountId = 1234556
+let AccountEmail = "tejas@123"
+var AccountPassword = "tejas123"
+AccountCity = "Jaipur"
+let AccountDet 
+
+
+AccountEmail = "Jaydeep@123"
+AccountPassword = "jaydeep123"
+AccountCity = "Hydrabad"
+
+console.table([AccountId,AccountEmail,AccountPassword,AccountCity,AccountDet]);
