79598ca955d220cc3e2a881bf4c8126f32369468
Tejas-Shinde-7038 <tejasshinde7038@gmail.com>
2025-08-05 09:28:45 +0000

    TypeOf Changes

---
+++ 01_JS_Prep/typeof.js
@@ -0,0 +1,14 @@
+let no = 10
+console.log(typeof(no));
+let stringNumber = String(no)
+console.log(typeof(stringNumber));
+let loggedin = 0
+let isloggedin = Boolean(loggedin)
+console.log(isloggedin);
+
+
+
+//number will covert easily like 10 converted into string
+//number who content letter in it will not converted output will show NAN
+//Boolean value true show in output "1" false "0"
+//1 change the type to boolean then it gives true and 0 gives false
