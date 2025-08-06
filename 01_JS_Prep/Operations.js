ea846209276347ab18ed771f5dd68361226a124d
Tejas-Shinde-7038 <tejasshinde7038@gmail.com>
2025-08-06 06:57:09 +0000

    Operations on js

---
+++ 01_JS_Prep/Operations.js
@@ -0,0 +1,22 @@
+let a = 2
+let b = 3
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);  //This operator calculates the result of (a) to the power (b). 
+console.log(a%b);
+
+
+str1 = "Tejas"
+str2 = "Kaustubh"
+str3 = str1+str2
+console.log(str3);
+
+console.log("2"+3);
+console.log(2+"3");
+console.log(2+"3"+"3");
+console.log(2+3+"4");
+
+
+
