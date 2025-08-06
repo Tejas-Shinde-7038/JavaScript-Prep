aef1692b00126a6e1f0212ec282888aa60497cf4
Tejas-Shinde-7038 <tejasshinde7038@gmail.com>
2025-08-06 07:15:05 +0000

    Push Component Sucessfullly

---
+++ 01_JS_Prep/ComparisonsDatatype.js
@@ -0,0 +1,14 @@
+// console.log(2>=1);
+// console.log(10<=10);
+// console.log(20<=12);
+// console.log(20!=12);
+
+// console.log("20"<12);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+console.log("2"==2);  //here datatype convertion done by js
+console.log("2"===2); //Here in "===" datatype conversion id not done by js
+
